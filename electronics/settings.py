2b3e60d7b902d3b2652f44c3a253cfa76371df27
shahul hameed <shahul66649@gmail.com>
2023-01-29 11:20:59 -1200

    heroku

--- electronics/settings.py
+++ electronics/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+
+#deploying on heroku
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware', #google
+    'whitenoise.middleware.WhiteNoiseMiddleware',             #heroku
 ]
 
 ROOT_URLCONF = 'electronics.urls'
@@ -128,6 +136,11 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),]
 
+#heroku 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorge'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -186,3 +199,5 @@ MEDIA_URL = '/media/'
 
 
 SITE_ID= 1
+
+django_heroku.settings(locals())
